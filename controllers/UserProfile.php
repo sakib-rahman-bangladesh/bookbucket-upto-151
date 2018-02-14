38ef46bb6c55aebbdd702a53be1021c0800c7cb4
Sakib Rahman <sakib.rahman.0000@gmail.com>
2018-02-15 02:04:59 +0600

    continuous intregeration: v1.0.0

--- controllers/UserProfile.php
+++ controllers/UserProfile.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Mostly Fluid - Quiz</title>
+    <title>Mostly Fluid - Pattern</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
